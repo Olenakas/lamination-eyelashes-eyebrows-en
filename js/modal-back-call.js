5fabb14c6aea128cc7b5b71361055f64103c51b0
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-07-23 15:50:09 +0200

    fixed open modal window

--- js/modal-back-call.js
+++ js/modal-back-call.js
@@ -1,14 +1,17 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-back-call-open]"),
-      closeModalBtn: document.querySelector("[data-modal-back-call-close]"),
-      modal: document.querySelector("[data-modal-back-call]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtns: document.querySelectorAll("[data-modal-back-call-open]"),
+    closeModalBtn: document.querySelector("[data-modal-back-call-close]"),
+    modal: document.querySelector("[data-modal-back-call]"),
+  };
+
+  refs.openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
